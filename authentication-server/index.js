12d6e9f33ecf9148e41cc40a6f43ca4a6453a5aa
Viresh khandelwal <vireshkhandelwal93@gmail.com>
2019-09-26 20:33:33 +0530

    installed drivers for mongoDB connection , created API to authenticate username

--- authentication-server/index.js
+++ authentication-server/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
+
+var MongoClient = require('mongodb').MongoClient;
+
 // app.get('/', (req, res) => {
 //     res.send('Welcome to Node API')
 // })
@@ -12,12 +15,21 @@ app.listen(3000, () => console.log('Example app listening on port 3000!'))
 //     res.json({'message': 'Hello World'})
 // })
 
-let usernames = ['viresh','charu','shweta','sakshi','samiksha','shriya','tushi']
 app.post('/authenticateUsername', bodyParser.json(), (req, res) => {
-    console.log(req.body);
-    if(usernames.indexOf(req.body.username)  !== -1){
-        res.send({userAuthenticated: true});
-    }else{
-        res.send({userAuthenticated: false});
-    }    
+    console.log(req.body);  
+    // Connect to the db
+    MongoClient.connect("mongodb://localhost:27017", function (err, client) {
+        if (err) throw err;
+        var db = client.db('authenticationDB');
+        db.collection('usernames', function (err, collection) {       
+            collection.find({ name: req.body.username}).toArray(function(err, items) {
+                if(err) throw err; 
+                console.log(items);                  
+                res.send({userAuthenticated: items.length !== 0});
+            });
+        });
+
+    });
+
+    
 })
